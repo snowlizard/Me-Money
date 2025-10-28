d546cdbd1b7d29430683ead7b03f9ffea17cfd19
snowlizard <illegal_burrito@live.com>
2025-10-27 21:13:23 -0700

    connecting to db

--- server/index.js
+++ server/index.js
@@ -1,4 +1,81 @@
-require('dotenv').config();
-
 const express = require('express');
 const app = express();
+
+const pool = require("./db");
+require('dotenv').config();
+
+app.use(express.json());
+
+app.get("/activities", async(req, res) => {
+    try {
+        const data = await pool.query('SELECT * FROM "Activities"');
+        res.json(data.rows);
+    } catch (error) {
+        console.log("BRUH" + error);
+        res.send("Error: something went wrong");
+    }
+});
+
+/**
+ * Category
+ */
+app.get('/category:id', async(req, res) => {
+        try {
+            const category_id = req.body.category_id;
+            const data = await pool.query('SELECT * FROM "Category" WHERE category_id = ($1)', [category_id]);
+            res.json(data.rows);
+        } catch (error) {
+            console.log(error);
+        }
+});
+app.get('/categories', async(req, res) => {
+        try {
+            const data = await pool.query('SELECT * FROM "Category" WHERE parent IS NULL');
+            res.json(data.rows);
+        } catch (error) {
+        console.log("BRUH " + error);
+        res.send("Error: something went wrong");
+        }
+});
+app.get('/subcategories', async(req, res) => {
+        try {
+            const data = await pool.query('SELECT * FROM "Category" WHERE parent IS NOT NULL');
+            res.json(data.rows);
+        } catch (error) {
+            console.log(error);
+        }
+});
+app.put('/category:id', async(req, res) => {
+        try {
+            const category = req.body.category;
+            const data = await pool.query('INSERT INTO "Category" (category_id, name, parent) VALUES ($1, $2, $3)',
+                [category.id, category.name, category.parent]);
+            res.json(data.rows);
+        } catch (error) {
+            console.log(error);
+        }
+});
+app.put('/category:id', async(req, res) => {
+        try {
+            const category = req.body.category;
+            const data = await pool.query('UPDATE "Category" SET name = ($2), parent = ($3)  WHERE category_id = ($1)',
+                [category.id, category.name, category.parent]
+            );
+            res.json(data.rows);
+        } catch (error) {
+            console.log(error);
+        }
+});
+app.delete('/category:id', async(req, res) => {
+        try {
+            const category_id = req.body.category_id;
+            const data = await pool.query('DELETE FROM "Category" WHERE category_id = ($1)', [category_id]);
+            res.json(data.rows);
+        } catch (error) {
+            console.log(error);
+        }
+});
+
+app.listen(5000, () => {
+    console.log("Server is listening on port " + 5000);
+});
