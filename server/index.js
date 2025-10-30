461a1e11044e560f639f8c866db1cd72bd23eec0
snowlizard <illegal_burrito@live.com>
2025-10-30 15:19:20 -0700

    adding routes and refactoring

--- server/index.js
+++ server/index.js
@@ -9,25 +9,6 @@ require('dotenv').config();
 app.use(express.json());
 app.use('/category', category);
 
-
-app.get('/categories', async(req, res) => {
-        try {
-            const data = await pool.query('SELECT * FROM category WHERE parent is NULL');
-            res.json(data.rows);
-        } catch (error) {
-            console.log(error);
-            res.send(error);
-        }
-});
-app.get('/subcategories', async(req, res) => {
-        try {
-            const data = await pool.query('SELECT * FROM category WHERE parent IS NOT NULL');
-            res.json(data.rows);
-        } catch (error) {
-            console.log(error);
-        }
-    });
-
 app.listen(5000, () => {
     console.log("Server is listening on port " + 5000);
 });
--- server/routes/category.js
+++ server/routes/category.js
@@ -2,6 +2,34 @@ const express = require('express');
 const router = express.Router({ mergeParams: true, strict: true});
 const pool = require("../db");
 
+router.get('/all', async(req, res) => {
+    try {
+        const data = await pool.query('SELECT * FROM category');
+        res.json(data.rows);
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
+});
+
+router.get('/categories', async(req, res) => {
+    try {
+        const data = await pool.query('SELECT * FROM category WHERE parent is NULL');
+        res.json(data.rows);
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
+});
+router.get('/subcategories', async(req, res) => {
+    try {
+        const data = await pool.query('SELECT * FROM category WHERE parent IS NOT NULL');
+        res.json(data.rows);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
 router.get('/:id', async(req, res) => {
         try {
             const category_id = req.params.id;
