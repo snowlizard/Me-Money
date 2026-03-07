26be739bd0f183256725eb6514f955f0350da1c8
snowlizard <illegal_burrito@live.com>
2026-03-06 20:10:08 -0800

    removed frontend react project

--- frontend/src/main.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './pages/Home';
-
-createRoot(document.getElementById('root')!).render(
-  <Home />
-)
